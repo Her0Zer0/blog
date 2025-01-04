a8809bd71d7c1ef35e425908bd30593d2b5179e3
her0zer0 <rs.herozero@gmail.com>
2025-01-04 14:01:34 -0600

    made changes to base in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     markdown: {},
-    site: 'https://Her0Zer0.github.io',
+    site: 'https://her0zer0.github.io',
     base: '/builtbitbybitblog'
 });
