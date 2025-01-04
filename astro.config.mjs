c83fd03b823294a607fc4eb593128f21de55bb83
her0zer0 <rs.herozero@gmail.com>
2025-01-04 13:57:20 -0600

    setup for git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        
-    }
+    markdown: {},
+    site: 'https://Her0Zer0.github.io',
+    base: '/builtbitbybitblog'
 });
