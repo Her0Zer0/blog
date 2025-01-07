7bfcac2e64a09344fbc7ce9cb26199e0860e6f7f
her0zer0 <rs.herozero@gmail.com>
2025-01-07 05:14:08 -0600

    changing 404 and post pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     markdown: {},
-    site: 'https://her0zer0.github.io'
+    site: 'https://her0zer0.github.io',
 });
